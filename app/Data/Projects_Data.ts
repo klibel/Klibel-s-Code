c3e433ae0651c85fc3235627e968e3593574be93
Klibel Romero <klibel.a.romero@gmail.com>
2026-06-26 11:14:20 -0400

    Actualización de servicios y optimización del portafolio

--- app/Data/Projects_Data.ts
+++ app/Data/Projects_Data.ts
@@ -20,6 +20,14 @@ export const PROJECTS_DATA: Project[] = [
     },
     {
         id: 2,
+        title: "Lisbeth Rivas Landing Page",
+        description: "Landing page personal para Lisbeth Rivas, diseñada con un enfoque moderno y responsivo. Tiene como objetivo presentar su portafolio y servicios de manera atractiva y profesional, destacando sus habilidades y experiencia.",
+        imageUrl: "/lisbeth-rivas-img.png",
+        link: "https://portafolio-lisbeth-rivas.vercel.app/",
+        technologies: ["Html", "CSS", "JavaScript", "GSAP"],
+    },
+    {
+        id: 3,
         title: "VeloKicks",
         description: "VeloKicks es un e-commerce showcase de calzado deportivo de alto rendimiento. Cuenta con una interfaz premium de diseño aerodinámico, un catálogo dinámico con transiciones fluidas mediante GSAP, filtros de búsqueda avanzada.",
         imageUrl: "/velokickslab.png",
@@ -27,7 +35,7 @@ export const PROJECTS_DATA: Project[] = [
         technologies: ["React", "Next.js", "GSAP", "TypeScript", "CSS"],
     },
     {
-        id: 3,
+        id: 4,
         title: "GeoMetrics — Dashboard Global de Países",
         description: "Aplicación web interactiva que consume la API de Rest Countries para analizar indicadores demográficos globales en tiempo real.",
         imageUrl: "/GeoMetrics.png",
@@ -35,7 +43,7 @@ export const PROJECTS_DATA: Project[] = [
         technologies: ["React", "TypeScript", "CSS", "vite", "API", "material-ui", "Bootstrap" ],
     },
     {
-        id: 4,
+        id: 5,
         title: "PokéDex",
         description: "Una web interactiva que sirve como enciclopedia digital completa sobre las criaturas Pokémon. Permite a los usuarios buscar, filtrar y explorar cada Pokémon.",
         imageUrl: "/PokeDex-Portada.png",
@@ -43,7 +51,7 @@ export const PROJECTS_DATA: Project[] = [
         technologies: ["HTML", "CSS", "JavaScript", "API"],
     },
     {
-        id: 5,
+        id: 6,
         title: "Asegura Aqui!",
         description: "Sitio web orientado a la gestión de seguros automotrices. Permite al usuario completar formularios interactivos y generar un PDF final con los datos ingresados, simulando el proceso real de emisión de una póliza.",
         imageUrl: "/Asegura-aqui-Portada.png",
@@ -51,7 +59,7 @@ export const PROJECTS_DATA: Project[] = [
         technologies: ["HTML", "Tailwind CSS", "JavaScript"],
     },
     {
-        id: 6,
+        id: 7,
         title: "Impostor FC",
         description: "App Android para jugar al Impostor Futbolero, Se basa en un juego donde se crean su perfil los participantes y aleatoriamente se selecciona uno como impostor.",
         imageUrl: "/impostorFC-portada.png",
@@ -59,7 +67,7 @@ export const PROJECTS_DATA: Project[] = [
         technologies: ["React Native","Expo","TypeScript", "CSS", ],
     },
     {
-        id: 7,
+        id: 8,
         title: "QQSV",
         description: "QQSV (Quién quiere ser venezolano) es una App android, Sobre conocimiento venezolano como tradiciones, frases, comidas y otras cosas",
         imageUrl: "/QQSV-portada.png",
@@ -67,7 +75,7 @@ export const PROJECTS_DATA: Project[] = [
         technologies: ["React Native","Expo","TypeScript", "CSS", ],
     },
     {
-        id: 8,
+        id: 9,
         title: "PadelStats Pro",
         description: "PadelStats Pro, Es una App android que te ayuda a generar partidas de Padel con tus amigos, Contiene las funcionalidades adecuada para generar enfrentamiento y llevar el control de puntos",
         imageUrl: "/PadelStatsPro.png",
